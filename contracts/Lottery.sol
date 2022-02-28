90f0e7dcd3ac144ebb56b89495c6c7cefe0757fe
Jaroslaw Drabek <jarekdrabek@gmail.com>
2022-02-28 14:49:20 +0100

    adding chainlink VRFConsumerBaseV2 as a randomness provider

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,14 +3,32 @@
 pragma solidity ^0.8.7;
 
 
-contract Lottery {
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Lottery is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+    LinkTokenInterface LINKTOKEN;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    address link = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords =  2;
+    uint64 subscriptionId = 638;
+
+    constructor() VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        LINKTOKEN = LinkTokenInterface(link);
+        owner = msg.sender;
+    }
+
     address public owner;
     address payable[] public couponBuyers;
     address payable public winner;
+    uint256 public randomNumber;
 
-    constructor()  {
-        owner = msg.sender;
-    }
 
     function returnTheWinningPool() private {
         winner.transfer(address(this).balance);
@@ -36,10 +54,22 @@ contract Lottery {
     }
 
     function finishLottery() public payable onlyOwner {
-        uint256 randomNumber = 17;
+        COORDINATOR.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+
+    }
+
+    function fulfillRandomWords(uint256, /* requestId */ uint256[] memory randomWords) internal override {
+        randomNumber = randomWords[0];
         uint256 indexOfWinner = randomNumber % couponBuyers.length;
         winner = couponBuyers[indexOfWinner];
         returnTheWinningPool();
         couponBuyers = new address payable[](0);
+
     }
 }
