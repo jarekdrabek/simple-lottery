9a175335993e4eb2d10c64714f65e3687fe62837
Jaroslaw Drabek <jarekdrabek@gmail.com>
2022-02-28 17:05:36 +0100

    randomness config from file

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,17 +10,18 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 contract Lottery is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface COORDINATOR;
     LinkTokenInterface LINKTOKEN;
-    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
-    address link = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
-    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    bytes32 keyHash;
     uint32 callbackGasLimit = 100000;
-    uint16 requestConfirmations = 3;
-    uint32 numWords =  2;
-    uint64 subscriptionId = 638;
+    uint16 requestConfirmations;
+    uint32 numWords = 2;
+    uint64 subscriptionId;
 
-    constructor() VRFConsumerBaseV2(vrfCoordinator) {
-        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-        LINKTOKEN = LinkTokenInterface(link);
+    constructor(address _vrfCoordinator, address _link, bytes32 _keyHash, uint16 _requestConfirmation, uint64 _subscriptionId) VRFConsumerBaseV2(_vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+        LINKTOKEN = LinkTokenInterface(_link);
+        keyHash = _keyHash;
+        requestConfirmations = _requestConfirmation;
+        subscriptionId = _subscriptionId;
         owner = msg.sender;
     }
 
